9323f114005423843f200fc741b7ce2637215635
winspeednl <sven@unixguru.nl>
2016-06-27 16:50:53 +0200

    Updated to LibZ v0.2

---
+++ Java/src/me/winspeednl/libz/entities/LibZ_Mob.java
@@ -0,0 +1,9 @@
+package me.winspeednl.libz.entities;
+
+
+public abstract class LibZ_Mob extends LibZ_Entity {
+	
+	public int moveSpeed = 1;
+	public boolean isAlive = true;
+	
+}
---
+++ Java/src/me/winspeednl/libz/entities/LibZ_Player.java
@@ -0,0 +1,8 @@
+package me.winspeednl.libz.entities;
+
+import me.winspeednl.libz.core.Input;
+
+public abstract class LibZ_Player extends LibZ_Mob {
+	
+	public abstract void input(Input input);
+}
