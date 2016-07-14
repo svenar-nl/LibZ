7f910a9baeaa3fbfc2c7ca775fb28f62e1ef43e1
Sven Arends <winspeednl@users.noreply.github.com>
2016-07-14 16:39:03 +0200

    Updated Font.java

--- Java/src/me/winspeednl/libz/screen/Font.java
+++ Java/src/me/winspeednl/libz/screen/Font.java
@@ -5,27 +5,22 @@ import me.winspeednl.libz.image.Image;
 public enum Font {
 	STANDARD("/Font/standard.png");
 	
-	public final int NUM_UNICODES = 59;
-	public int[] offsets = new int[NUM_UNICODES];
-	public int[] widths = new int[NUM_UNICODES];
+	public final int NumberUnicodes = 59;
+	public int[] offsets = new int[NumberUnicodes];
+	public int[] widths = new int[NumberUnicodes];
 	public Image image;
 	
 	Font(String path) {
 		image = new Image(path);
-		
 		int unicode = -1;
-	
 		for(int x = 0; x < image.width; x++) {
 			int color = image.imagePixels[x];
-			
 			if(color == 0xff0000ff) {
 				unicode++;
 				offsets[unicode] = x;
 			}
-			
-			if(color == 0xffffff00) {
+			if(color == 0xffffff00)
 				widths[unicode] = x - offsets[unicode];
-			}
 		}
 	}
 }
