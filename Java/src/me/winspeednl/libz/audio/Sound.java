185f198103f53e347911a1b6c7c96e5c59f89556
winspeednl <sven@unixguru.nl>
2016-07-30 22:27:37 +0200

    Changed playback method. Added "isPlaying()" function

--- Java/src/me/winspeednl/libz/audio/Sound.java
+++ Java/src/me/winspeednl/libz/audio/Sound.java
@@ -1,28 +1,47 @@
 package me.winspeednl.libz.audio;
 
-import java.applet.Applet;
-import java.applet.AudioClip;
+import java.io.BufferedInputStream;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
 
 public class Sound {
-	private AudioClip clip;
-
-	public Sound(String name) {
-		try {
-			clip = Applet.newAudioClip(Sound.class.getResource(name));
-		} catch (Throwable e) {
+	AudioInputStream audioInputStream;
+    Clip clip;
+    
+    public Sound(String path) {
+    	try {
+    		BufferedInputStream myStream = new BufferedInputStream(getClass().getResourceAsStream(path));
+    		audioInputStream = AudioSystem.getAudioInputStream(myStream);
+    		
+			DataLine.Info info = new DataLine.Info(Clip.class, audioInputStream.getFormat());
+	        clip = (Clip)AudioSystem.getLine(info);
+			clip.open(audioInputStream);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
-
-	public void play() {
-		try {
-			new Thread() {
-				public void run() {
-					clip.play();
-				}
-			}.start();
-		} catch (Throwable e) {
-			e.printStackTrace();
-		}
-	}
+    }
+    
+    public void play() {
+    	clip.setFramePosition(0);
+    	clip.start();
+    }
+    
+    public void loop() {
+    	clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+    
+    public void loop(int count) {
+    	clip.loop(count);
+    }
+    
+    public void stop() {
+    	clip.stop();
+    }
+    
+    public boolean isPlaying() {
+    	return clip.isRunning();
+    }
 }
